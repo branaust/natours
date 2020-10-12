b40a10665f465c3eaa6ed620aaa38ef65451731a
Brandon Austin <61210295+branaus@users.noreply.github.com>
2020-10-12 08:57:06 -0700

    Building the Tour Overview

---
+++ controllers/viewsController.js
@@ -0,0 +1,22 @@
+const Tour = require('../models/tourModel.js');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build out template
+
+  // 3) Render that template using the data from step 1
+
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+};
