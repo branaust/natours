111b40094114dcfd50aeef886908293c9fb0400a
Brandon Austin <61210295+branaus@users.noreply.github.com>
2020-10-14 11:58:27 -0700

    Mapbox Implementation

--- app.js
+++ app.js
@@ -29,7 +29,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'https:', 'http:', 'data:', 'ws:'],
+      baseUri: ["'self'"],
+      fontSrc: ["'self'", 'https:', 'http:', 'data:'],
+      scriptSrc: ["'self'", 'https:', 'http:', 'blob:'],
+      styleSrc: ["'self'", 'https:', 'http:', 'unsafe-inline'],
+    },
+  })
+);
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
---
+++ public/js/mapbox.js
@@ -0,0 +1,8 @@
+/* esling-disable */
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYnJhbmF1c3QiLCJhIjoiY2tnOHVibGllMDVuYjJ5cHFmbmpucndwZSJ9.KBzYV-QSHSChEb6QJpNpdg';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+});
