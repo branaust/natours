daaed0c4b4593a0e550f28c294178be5b2d0576b
Brandon Austin <61210295+branaus@users.noreply.github.com>
2020-10-14 22:09:34 -0700

    Implementing Mapbox

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,8 +1,49 @@
 /* eslint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYnJhbmF1c3QiLCJhIjoiY2tnOHVibGllMDVuYjJ5cHFmbmpucndwZSJ9.KBzYV-QSHSChEb6QJpNpdg';
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v11',
+  style: 'mapbox://styles/branaust/ckgac0pdn0j3e19nq9uy9he0m',
+  scrollZoom: false,
+  // center: [-118.113491, 34.111745],
+  // zoom: 4,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add Popup
+  new mapboxgl.Popup({
+    offset: 40,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
 });
