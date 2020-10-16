fa8f48a9a2db8fa7580eef0a93cc50aafa245ddb
Brandon Austin <61210295+branaus@users.noreply.github.com>
2020-10-16 16:17:54 -0700

    Logging in the user with the API

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -29,6 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
+
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
@@ -36,7 +38,7 @@ app.use(
       baseUri: ["'self'"],
       fontSrc: ["'self'", 'https:', 'http:', 'data:'],
       scriptSrc: ["'self'", 'https:', 'http:', 'blob:'],
-      styleSrc: ["'self'", 'https:', 'http:', 'unsafe-inline'],
+      styleSrc: ["'self'", 'https:', 'http:', "'unsafe-inline'"],
     },
   })
 );
@@ -61,6 +63,7 @@ app.use(
     limit: '10kb',
   })
 );
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -87,7 +90,7 @@ app.use(globalErrorHandler);
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.headers);
+  console.log(req.cookies);
 
   next();
 });
--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,18 @@
 /* eslint-disable */
 
 const login = async (email, password) => {
-  console.log(email, password);
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://127.0.0.1:8000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
-        email,
-        password,
+        email: email,
+        password: password,
       },
     });
     console.log(res);
   } catch (err) {
-    console.log(err.response.data);
+    console.log(err);
   }
 };
 
@@ -21,5 +20,5 @@ document.querySelector('.form').addEventListener('submit', (e) => {
   e.preventDefault();
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-  login({ email, password });
+  login(email, password);
 });
