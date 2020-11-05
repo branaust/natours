f6f73058087fb1919a1eafb2c4cc0e0b437093f6
Brandon Austin <61210295+branaus@users.noreply.github.com>
2020-11-04 20:04:55 -0800

    Modeling the bookings

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,15 +1,24 @@
 /* eslint-disable */
 import axios from 'axios';
+import { showAlert } from './alerts';
 const stripe = Stripe(
   'pk_test_51HhRPiCrICTmYAs3q4Zk3Sc4dEv1VRyf3ATetFnWit2UVcCfjmkulRqeh19HGJNSc547YPnzf00CwnQ2szS0j5rD00b345RVdJ'
 );
 
 export const bookTour = async (tourId) => {
-  // 1) Get checkout session from API
-  const session = await axios(
-    `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`
-  );
-  console.log(session);
+  try {
+    // 1) Get checkout session from API
+    const session = await axios(
+      `http://localhost:8000/api/v1/bookings/checkout-session/${tourId}`
+    );
+    console.log(session);
 
-  // 2) Create checkout form + charge credit card
+    // 2) Create checkout form + chanre credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
 };
